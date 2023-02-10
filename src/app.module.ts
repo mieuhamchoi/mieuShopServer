c16b60a6081606fcb0eb6614dc884523c0ac19da
Mieu <mieuhamchoi@gmail.com>
2023-02-10 11:43:00 +0700

    create provider connect database use libirary mysql2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//database connect use mysql2
+import { DatabaseConnection } from './database.providers';
+
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseConnection],
 })
 export class AppModule {}
---
+++ src/database.providers.ts
@@ -0,0 +1,20 @@
+import { createConnection, Connection } from 'mysql2';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DatabaseConnection {
+  private connection: Connection;
+
+  constructor() {
+    this.connection = createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'mieushopdb'
+    });
+  }
+
+  getConnection(): Connection {
+    return this.connection;
+  }
+}
